f084261492c10d56f4d307c240f9dae816fe8899
Nadeem Siddique <nadeem@sprinto.com>
2022-07-01 21:57:24 +0530

    ADded a terminal interface

--- src/components/app.js
+++ src/components/app.js
@@ -2,61 +2,19 @@ import { useEffect, useRef, useState } from "preact/hooks";
 import Tabs from "./tabs";
 import TabContent from "./tab-content";
 import AnimatedCursor from "react-animated-cursor";
+import Router from "preact-router";
+import Interface from "../routes/interface";
+import Terminal from "../routes/terminal";
 const App = () => {
-  // const [dots, setDots] = useState([new Dot()]);
-  const cursor = useRef();
-  const [mouse, setMouse] = useState({ x: 0, y: 0 });
-
-  const addCursor = () => {
-    cursor.current.classList.remove("cursor-default");
-    cursor.current.classList.add("cursor-active");
-  };
-
-  const removeCursor = () => {
-    cursor.current.classList.remove("cursor-active");
-    cursor.current.classList.add("cursor-default");
-  };
-
-  useEffect(() => {
-    var links = document.querySelectorAll(
-      'a, button, label, input[type="button"], input[type="submit"]'
-    );
-
-    addEventListener("mouseenter", addCursor);
-    addEventListener("mouseleave", removeCursor);
-
-    addEventListener("mousemove", (event) => {
-      // console.log({ event });
-      // console.log({ cursor })
-      console.log({ x: event.pageX, y: event.pageY });
-      cursor.current.style.left = event.pageX + "px";
-      cursor.current.style.top = event.pageY + "px";
-      // setMouse({ x: event.pageX, y: event.pageY });
-    });
-    return () => {
-      removeEventListener("mousemove");
-    };
-  }, []);
-
+  const [isAgreed, setAgree] = useState(false);
   return (
-    // <div className="app">
-    <div id="frame" className="frame">
-      <div className="piece output container">
-        <div className="pipboy">
-          <Tabs />
-          <TabContent />
-        </div>
-        <div class="piece glow noclick"></div>
-        <div class="piece scanlines noclick"></div>
-      </div>
-      <div ref={cursor} className="cursor">
-        <img
-          src="https://cybersandbox.ca/resources/nv-cursor.png"
-          height={28}
-        />
-      </div>
+    <div>
+      {isAgreed ? <Interface /> : <Terminal onAgree={() => setAgree(true)} />}
+      {/* <Router>
+        <Terminal path="/" />
+        <Interface path="/interface" />
+      </Router> */}
     </div>
-    // </div>
   );
 };
 
---
+++ src/routes/interface/index.js
@@ -0,0 +1,62 @@
+import { useEffect, useRef, useState } from "preact/hooks";
+import Tabs from "../../components/tabs";
+import TabContent from "../../components/tab-content";
+
+const Interface = () => {
+  // const [dots, setDots] = useState([new Dot()]);
+  const cursor = useRef();
+
+  const addCursor = () => {
+    cursor.current.classList.remove("cursor-default");
+    cursor.current.classList.add("cursor-active");
+  };
+
+  const removeCursor = () => {
+    cursor.current.classList.remove("cursor-active");
+    cursor.current.classList.add("cursor-default");
+  };
+
+  useEffect(() => {
+    var links = document.querySelectorAll(
+      'a, button, label, input[type="button"], input[type="submit"]'
+    );
+
+    addEventListener("mouseenter", addCursor);
+    addEventListener("mouseleave", removeCursor);
+
+    addEventListener("mousemove", (event) => {
+      // console.log({ event });
+      // console.log({ cursor })
+      console.log({ x: event.pageX, y: event.pageY });
+      cursor.current.style.left = event.pageX + "px";
+      cursor.current.style.top = event.pageY + "px";
+      // setMouse({ x: event.pageX, y: event.pageY });
+    });
+    return () => {
+      removeEventListener("mousemove");
+    };
+  }, []);
+
+  return (
+    // <div className="app">
+    <div id="frame" className="frame">
+      <div className="piece output container">
+        <div className="pipboy">
+          <Tabs />
+          <TabContent />
+        </div>
+        <div class="piece glow noclick"></div>
+        <div class="piece scanlines noclick"></div>
+      </div>
+      <div ref={cursor} className="cursor">
+        <img
+          src="https://cybersandbox.ca/resources/nv-cursor.png"
+          height={28}
+        />
+      </div>
+    </div>
+    // </div>
+  );
+};
+
+export default Interface;
